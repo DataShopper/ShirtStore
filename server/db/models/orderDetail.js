c17f3d2aabaf61f0f5e81de516f36a4a3f33a662
SkwibHub <eidolongroups@gmail.com>
2019-01-08 11:49:57 -0800

    Chore: Created OrderDetail Model.

---
+++ server/db/models/orderDetail.js
@@ -0,0 +1,23 @@
+const db = require('../db');
+const Sequelize = require('sequelize');
+
+const OrderDetail = db.define('orderdetail', {
+  Quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  totalPrice: {
+    type: Sequelize.DECIMAL(10, 2),
+    defaultValue: null,
+  },
+  size: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  color: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = OrderDetail;
