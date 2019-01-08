dc495f490ec2b292010767c02f3657f25749ff4f
Thomas Weidner <tomweidner103@gmail.com>
2019-01-08 14:30:15 -0800

    tweaked and tested successfully orders route

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,14 +1,15 @@
 const router = require('express').Router()
 const {Order} = require('../db/models')
+const {OrderDetail} = require('../db/models')
 
-router.get('/:userId/', async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
-    const orders = Order.findAll({
+    const orders = await Order.findAll({
       where: {
         userId: req.params.userId,
         bought: true
       },
-      include: ['orderDetails']
+      include: [OrderDetail]
     })
     res.json(orders)
   } catch (error) {
