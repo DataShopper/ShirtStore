240fa767aad09dd11a7ef7017d7fc11342acae27
Thomas Weidner <tomweidner103@gmail.com>
2019-01-08 16:10:36 -0800

    working past orders route, component, redux

---
+++ client/components/Orders/PurchasedOrders.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {getPurchasedOrders} from '../../store/orders'
+
+class PurchasedOrders extends React.Component {
+  constructor() {
+    super()
+  }
+  async componentDidMount() {
+    await this.props.getPurchasedOrders(this.props.id)
+  }
+  render() {
+    console.log(this.props.id)
+    const {purchasedOrders} = this.props || []
+    return (
+      <div>
+        <ul>
+          {purchasedOrders.map(order => {
+            return <li key={order.id}>{order.totalPrice}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    purchasedOrders: state.purchasedOrders
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getPurchasedOrders: userId => dispatch(getPurchasedOrders(userId))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PurchasedOrders)
