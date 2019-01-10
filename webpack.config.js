7add25f1e694cfda05102f72820dc0f9ed5d9fbe
SkwibHub <eidolongroups@gmail.com>
2019-01-10 13:26:53 -0800

    'Chore: Installing semantic and making the installation work'

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const isDev = process.env.NODE_ENV === 'development'
 
+const path = require('path')
+
 module.exports = {
   mode: isDev ? 'development' : 'production',
   entry: [
@@ -20,6 +22,26 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'resolve-url-loader'],
+        include: [path.join(__dirname, 'src'), /node_modules/]
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      },
+      {
+        test: [/\.eot$/, /\.ttf$/, /\.svg$/, /\.woff$/, /\.woff2$/],
+        loader: require.resolve('file-loader'),
+        options: {
+          name: '/static/media/[name].[hash:8].[ext]'
+        }
       }
     ]
   }
