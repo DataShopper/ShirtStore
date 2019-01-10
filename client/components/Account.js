9aaf4f9bbab82a53205920d8cdcf430206af91ad
Ashley Smith <smitham85@gmail.com>
2019-01-09 18:23:02 -0800

    feature: displays and edits user account info issue#3

---
+++ client/components/Account.js
@@ -0,0 +1,113 @@
+import axios from 'axios'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class Account extends Component {
+  constructor() {
+    super()
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({
+      firstName: this.props.user.firstName,
+      lastName: this.props.user.lastName,
+      email: this.props.user.email,
+      id: this.props.user.id
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault()
+    await axios.put('/api/users/account', this.state)
+
+    //this.props.history.push('/students');
+  }
+
+  render() {
+    //console.log(this.props, 'hey')
+    return (
+      <div>
+        <h3
+          style={{
+            textAlign: 'center'
+          }}
+        >
+          Account Profile
+        </h3>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-container">
+            <label className="form-row">
+              First Name
+              <input
+                onChange={this.handleChange}
+                name="firstName"
+                type="text"
+                value={this.state.firstName}
+                required
+              />
+            </label>
+            <label className="form-row">
+              Last Name
+              <input
+                onChange={this.handleChange}
+                name="lastName"
+                type="text"
+                value={this.state.lastName}
+                required
+              />
+            </label>
+            <label className="form-row">
+              Email
+              <input
+                onChange={this.handleChange}
+                name="email"
+                type="email"
+                value={this.state.email}
+                required
+              />
+            </label>
+          </div>
+          <button
+            type="submit"
+            style={{
+              float: 'right',
+              marginRight: '7px'
+            }}
+          >
+            {' '}
+            Save Changes
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+import {submitEditedUser} from '../store/user'
+
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    editUser: newuseredit => dispatch(submitEditedUser(newuseredit))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Account)
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -14,6 +14,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           <Link to="/home">Home</Link>
           <Link to="/products">Products</Link>
           <Link to="/purchasedOrders">Past Orders</Link>
+          <Link to="/account"> Account </Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
