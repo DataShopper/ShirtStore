2bff8302fc515b3d3f7c1908c59afa6f5670d6e7
Thomas Weidner <tomweidner103@gmail.com>
2019-01-10 23:49:24 -0800

    tiny admin fix

--- server/api/products.js
+++ server/api/products.js
@@ -4,6 +4,7 @@ const {Product} = require('../db/models')
 //Get all products /api/products
 router.get('/', async (req, res, next) => {
   try {
+    console.log(req.user)
     const products = await Product.findAll()
     res.json(products)
   } catch (err) {
@@ -13,10 +14,12 @@ router.get('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    console.log('boooodddyyy', req.body)
-    const product = await Product.create(req.body)
-    console.log('product', product)
-    res.json(product)
+    if (req.user.admin) {
+      const product = await Product.create(req.body)
+      res.json(product)
+    } else {
+      res.sendStatus(401)
+    }
   } catch (err) {
     next(err)
   }
