6a042f36c467289ed9ff71f38df281b4c2119a82
SkwibHub <eidolongroups@gmail.com>
2019-01-14 12:53:07 -0800

    'Fix: Semantic UI fixes'

--- client/components/ProductForm.js
+++ client/components/ProductForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Input, Button} from 'semantic-ui-react'
 
 const ProductForm = props => {
   if (props.state) {
@@ -12,130 +13,134 @@ const ProductForm = props => {
     const category = props.state.category
 
     return (
-      <form className='ui-form' onSubmit={handleSubmit}>
-        <label htmlFor="price">
-          <input
-            className='field'
-            name="price"
-            type="text"
-            placeholder="price"
-            onChange={handleChange}
-            value={price}
-          />
-        </label>
-        <label htmlFor="name">
-          <input
-            className='field'
-            name="name"
-            type="text"
-            placeholder="name"
-            onChange={handleChange}
-            value={name}
-          />
-        </label>
-        <label htmlFor="color">
-          <input
-            className='field'
-            name="color"
-            type="text"
-            placeholder="color"
-            onChange={handleChange}
-            value={color}
-          />
-        </label>
-        <label htmlFor="description">
-          <input
-            className='field'
-            name="description"
-            type="text"
-            placeholder="desc"
-            onChange={handleChange}
-            value={description}
-          />
-        </label>
-        <label htmlFor="sizes">
-          <input
-            className='field'
-            name="sizes"
-            type="text"
-            placeholder="size"
-            onChange={handleChange}
-            value={sizes}
-          />
-        </label>
-        <label htmlFor="category">
-          <input
-            className='field'
-            name="category"
-            type="text"
-            placeholder="category"
-            onChange={handleChange}
-            value={category}
-          />
-        </label>
-        <button>Add Item</button>
-      </form>
+      <div className="ui two column doubling stackable centered padded grid row container">
+        <form className="ui-form" onSubmit={handleSubmit}>
+          <label htmlFor="price">
+            <Input
+              className="field"
+              name="price"
+              type="text"
+              placeholder="price"
+              onChange={handleChange}
+              value={price}
+            />
+          </label>
+          <label htmlFor="name">
+            <Input
+              className="field"
+              name="name"
+              type="text"
+              placeholder="name"
+              onChange={handleChange}
+              value={name}
+            />
+          </label>
+          <label htmlFor="color">
+            <Input
+              className="field"
+              name="color"
+              type="text"
+              placeholder="color"
+              onChange={handleChange}
+              value={color}
+            />
+          </label>
+          <label htmlFor="description">
+            <Input
+              className="field"
+              name="description"
+              type="text"
+              placeholder="desc"
+              onChange={handleChange}
+              value={description}
+            />
+          </label>
+          <label htmlFor="sizes">
+            <Input
+              className="field"
+              name="sizes"
+              type="text"
+              placeholder="size"
+              onChange={handleChange}
+              value={sizes}
+            />
+          </label>
+          <label htmlFor="category">
+            <Input
+              className="field"
+              name="category"
+              type="text"
+              placeholder="category"
+              onChange={handleChange}
+              value={category}
+            />
+          </label>
+          <Button className="ui centered">ADD ITEM</Button>
+        </form>
+      </div>
     )
   } else {
     const {handleChange, handleSubmit} = props
     return (
-      <form className='ui form' onSubmit={handleSubmit}>
-        <label htmlFor="price">
-          <input
-            className='field'
-            name="price"
-            type="text"
-            placeholder="price"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="name">
-          <input
-            className='field'
-            name="name"
-            type="text"
-            placeholder="name"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="color">
-          <input
-            className='field'
-            name="color"
-            type="text"
-            placeholder="color"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="description">
-          <input
-            className='field'
-            name="description"
-            type="text"
-            placeholder="desc"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="sizes">
-          <input
-            className='field'
-            name="sizes"
-            type="text"
-            placeholder="size"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="category">
-          <input
-            className='field'
-            name="category"
-            type="text"
-            placeholder="category"
-            onChange={handleChange}
-          />
-        </label>
-        <button>Add Item</button>
-      </form>
+      <div className="ui two column double stackable container">
+        <form className="ui form" onSubmit={handleSubmit}>
+          <label htmlFor="price">
+            <Input
+              className="field"
+              name="price"
+              type="text"
+              placeholder="price"
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="name">
+            <Input
+              className="field"
+              name="name"
+              type="text"
+              placeholder="name"
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="color">
+            <Input
+              className="field"
+              name="color"
+              type="text"
+              placeholder="color"
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="description">
+            <Input
+              className="field"
+              name="description"
+              type="text"
+              placeholder="desc"
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="sizes">
+            <Input
+              className="field"
+              name="sizes"
+              type="text"
+              placeholder="size"
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="category">
+            <Input
+              className="field"
+              name="category"
+              type="text"
+              placeholder="category"
+              onChange={handleChange}
+            />
+          </label>
+          <Button className="ui centered">ADD ITEM</Button>
+        </form>
+      </div>
     )
   }
 }
