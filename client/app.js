524b38e5749ca2fa6b4484b86269407165dc49d1
Andrew Smith <asmith7592@yahoo.com>
2019-01-14 20:36:26 -0800

    chore: remove useless comments and unused imports

--- client/app.js
+++ client/app.js
@@ -1,28 +1,5 @@
-// import React, {Component} from 'react'
-
-// import {Elements, StripeProvider} from 'react-stripe-elements';
-// import {Navbar} from './components'
-// import Routes from './routes'
-
-// class App extends Component {
-//   render() {
-
-//     return (
-//       <div>
-//         <StripeProvider apiKey='pk_test_QJqh7Hj9VDs05MOowKlYcWkC'>
-//         <Navbar />
-//         <Routes />
-//         </StripeProvider>
-//       </div>
-//     )
-//   }
-// }
-
-// export default App
-
 import React, {Component} from 'react'
-import {Elements, StripeProvider} from 'react-stripe-elements'
-import CheckoutContainer from './components/CheckoutContainer'
+import {StripeProvider} from 'react-stripe-elements'
 import {Navbar} from './components'
 import Routes from './routes'
 
